b18e0b3ee1bfc1b3206b5e4c6d447202500671c9
Sahilkumarrr13 <sahilkumar2299@gmail.com>
2021-11-06 17:24:26 +0530

    Added href target to new page for Github logo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -22,7 +22,7 @@ function Footer(){
 				</span>
 				<span className="githubico">
 				<IconButton color="primary">
-				<a href="https://github.com/PEC-Programmers-Arena/Wonder4Kids2" text-decoration="none"><GitHubIcon fontSize="large" className="footerIcon"/>&nbsp;GitHub</a>
+				<a href="https://github.com/PEC-Programmers-Arena/Wonder4Kids2" target="_blank" text-decoration="none"><GitHubIcon fontSize="large" className="footerIcon"/>&nbsp;GitHub</a>
 				</IconButton>
 				</span>
 				{/* </center> */}
