e720d55f71df8983ac19aeae1e396010c68804bb
Rahul5430 <rahul213himani@gmail.com>
2021-11-06 18:16:17 +0530

    added target attribute in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // import { Box, Link, Stack, Text } from '@chakra-ui/react';
-import "./Footer.css";
+import './Footer.css';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import IconButton from '@mui/material/IconButton';
 // import TabScrollButton from '@mui/material/TabScrollButton';
@@ -8,39 +8,48 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import EmailIcon from '@mui/icons-material/Email';
 
-
-function Footer(){
-	return(
+function Footer() {
+	return (
 		<>
-			<div className="grid">
-				<div className="githubinfo">
-				<span className="ProjectInfoMobile">
-				&nbsp;Want to Contribute? &nbsp;
-				</span>
-				<span className="ProjectInfoDesktop">
-				This is an Open Source Project &nbsp;
-				</span>
-				<span className="githubico">
-				<IconButton color="primary">
-				<a href="https://github.com/PEC-Programmers-Arena/Wonder4Kids2" text-decoration="none"><GitHubIcon fontSize="large" className="footerIcon"/>&nbsp;GitHub</a>
-				</IconButton>
-				</span>
-				{/* </center> */}
+			<div className='grid'>
+				<div className='githubinfo'>
+					<span className='ProjectInfoMobile'>
+						&nbsp;Want to Contribute? &nbsp;
+					</span>
+					<span className='ProjectInfoDesktop'>
+						This is an Open Source Project &nbsp;
+					</span>
+					<span className='githubico'>
+						<IconButton color='primary'>
+							<a
+								href='https://github.com/PEC-Programmers-Arena/Wonder4Kids2'
+								text-decoration='none'
+								target="_blank" 
+								rel="noreferrer"
+							>
+								<GitHubIcon
+									fontSize='large'
+									className='footerIcon'
+								/>
+								&nbsp;GitHub
+							</a>
+						</IconButton>
+					</span>
+					{/* </center> */}
 				</div>
-				<div className="Contacts">
+				<div className='Contacts'>
 					<center>Contact Us</center>
-				
-					<div className="groupContacts">
-						<IconButton>	
-							<InstagramIcon color="primary" fontSize="large"/>
+
+					<div className='groupContacts'>
+						<IconButton>
+							<InstagramIcon color='primary' fontSize='large' />
 						</IconButton>
 						<IconButton>
-							<FacebookIcon color="primary" fontSize="large"/>
+							<FacebookIcon color='primary' fontSize='large' />
 						</IconButton>
 						<IconButton>
-							<EmailIcon color="primary" fontSize="large"/>
+							<EmailIcon color='primary' fontSize='large' />
 						</IconButton>
-						
 
 						{/* <IconButton color="primary">
 							<TabScrollButton orientation="vertical" direction="left"/>
@@ -48,9 +57,8 @@ function Footer(){
 					</div>
 				</div>
 			</div>
-
 		</>
-	)
-};
+	);
+}
 
 export default Footer;
